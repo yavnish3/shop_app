64a92d775235597b7c87aee1c77287c65067aa06
yavnish3 <yavnish3@gmail.com>
2021-05-15 13:38:21 +0530

    commit

---
+++ lib/provider/cart.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  double get totalAmount {
+    double total = 0.0;
+    _items.forEach((key, value) {
+      total += value.price * value.quantity;
+    });
+    return total;
+  }
+
+  void addItem(String productId, String title, double price) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (value) => CartItem(
+                id: value.id,
+                title: value.title,
+                quantity: value.quantity + 1,
+                price: value.price,
+              ));
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+}
